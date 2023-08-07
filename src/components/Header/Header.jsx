d73a5285de750d8d820ea009e0f04040a4d7f382
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-08-07 17:27:16 +0530

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <span className="header__logo">QuadB Movies</span>
+    </header>
+  );
+};
+
+export default Header;
